bf392d84d43c87de1614dd32479d43e5bda70bdf
SQRPI <sqr_pi@163.com>
2020-02-22 19:59:53 +0800

    fix bug for value of 碳

--- MaterialPlanning.py
+++ MaterialPlanning.py
@@ -141,7 +141,7 @@ class MaterialPlanning(object):
 
 
         # To construct mapping from stage id to stage names and vice versa.
-        self.stage_array =[x+'-'+y for x in ['LS'] for y in '12345']
+        self.stage_array =[]
         for k,v in stage_dct.items():
             if v not in self.banned_stages:
                 self.stage_array.append(v)
@@ -168,7 +168,7 @@ class MaterialPlanning(object):
                 pass
 
         # 添加LS, CE, S4-6, S5-2的掉落
-        self.cost_lst[0:5] = [10,15,20,25,30]
+        #self.cost_lst[0:5] = [10,15,20,25,30]
 #        self.cost_lst[self.stage_dct_rv['1-7']] = 9
         for k, stage in enumerate(self.stage_array):
             self.probs_matrix[k, self.item_dct_rv['龙门币']] = self.cost_lst[k]*12
@@ -312,7 +312,6 @@ class MaterialPlanning(object):
 
         return solution, dual_solution, excp_factor
 
-
     def get_plan(self, requirement_dct, deposited_dct={},
                  print_output=False, outcome=False, gold_demand=True, exp_demand=True):
         """
@@ -421,6 +420,7 @@ class MaterialPlanning(object):
         self.item_value['寻访凭证'] = self.costLimit * 600 / 180
         self.item_value['芯片助剂'] = self.item_value['采购凭证'] * 90
         self.item_value['招聘许可'] = (20*self.公招出四星的概率+10)*self.item_value['糖组']/Price['糖组']+38/258*600/180*self.costLimit*self.公招出四星的概率 - self.item_value['龙门币']*774
+        self.item_value['碳'] = (self.item_value['家具零件']*4-200*self.item_value['龙门币'])/(1-0.5*self.ConvertionDR)
 
         for group in self.values:
             group["items"] = sorted(group["items"], key=lambda k: float(k['value']), reverse=True)
@@ -652,7 +652,6 @@ class MaterialPlanning(object):
 
     def output_credit(self):
         self.creditEffect = dict()
-        self.item_value['碳'] = (self.item_value['家具零件']*4-200*self.item_value['龙门币'])/(1-0.5*self.ConvertionDR)
         for item, value in Credit.items():
             self.creditEffect[item] = self.item_value[item]/value
 
@@ -703,9 +702,12 @@ class MaterialPlanning(object):
             print('关卡%s, 材料%s, 模式错误添加失败'%(stage, item))
 
     def update_stage_processing(self, stage_name: str, cost: int):
-        self.stage_array.append(stage_name)
-        self.stage_dct_rv.update({stage_name: len(self.stage_array)-1})
-        self.cost_lst = np.append(self.cost_lst, cost)
+        if stage_name not in self.stage_array:
+            self.stage_array.append(stage_name)
+            self.stage_dct_rv.update({stage_name: len(self.stage_array)-1})
+            self.cost_lst = np.append(self.cost_lst, cost)
+        else:
+            self.cost_lst[self.stage_dct_rv[stage_name]] = cost
 
     def update_droprate(self):
         self.update_droprate_processing('S4-6', '龙门币', 3228)
@@ -835,8 +837,17 @@ class MaterialPlanning(object):
         self.update_stage_processing('PR-C-2', 36)
         self.update_stage_processing('PR-D-1', 18)
         self.update_stage_processing('PR-D-2', 36)
-
-#        self.update_stage_processing('AP-5', 30)
+        self.update_stage_processing('CE-1', 10)
+        self.update_stage_processing('CE-2', 15)
+        self.update_stage_processing('CE-3', 20)
+        self.update_stage_processing('CE-4', 25)
+        self.update_stage_processing('CE-5', 30)
+        self.update_stage_processing('LS-1', 10)
+        self.update_stage_processing('LS-2', 15)
+        self.update_stage_processing('LS-3', 20)
+        self.update_stage_processing('LS-4', 25)
+        self.update_stage_processing('LS-5', 30)
+        self.update_stage_processing('AP-5', 30)
 
 
 
--- update_db.py
+++ update_db.py
@@ -21,50 +21,51 @@ Event_Stages = []
 update_time = parser.parse(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
 
 def update_collection(collection):
-  for item in collection.find():
-      if 'credit_store_value' in item:
-          if item['name'] in mp.best_stage:
-              collection.update_one({'_id': item['_id']},
-                  {'$set': {'credit_store_value': Decimal128('%.3f'%(100*mp.creditEffect[item['name']])),
-                            'Notes': mp.Notes[item['name']],
-                            'lowest_ap_stages': mp.best_stage[item['name']]['lowest_ap_stages'],
-                            'balanced_stages': mp.best_stage[item['name']]['balanced_stages'],
-                            'drop_rate_first_stages': mp.best_stage[item['name']]['drop_rate_first_stages'],
-                            'last_updated': update_time}
-                  })
-          else:
-              collection.update_one({'_id': item['_id']},
-                  {'$set': {'credit_store_value': Decimal128('%.3f'%(100*mp.creditEffect[item['name']])),
-                            'Notes': mp.Notes[item['name']],
-                            'lowest_ap_stages': [{}],
-                            'balanced_stages': [{}],
-                            'drop_rate_first_stages': [{}],
-                            'last_updated': update_time}
-                  })
-      if 'green_ticket_value' in item:
-          if item['name'] in mp.best_stage:
-              collection.update_one({'_id': item['_id']},
-                  {'$set': {'green_ticket_value': Decimal128('%.3f'%(mp.greenTickets[item['name']])),
-                            'Notes': mp.Notes[item['name']],
-                            'lowest_ap_stages': mp.best_stage[item['name']]['lowest_ap_stages'],
-                            'balanced_stages': mp.best_stage[item['name']]['balanced_stages'],
-                            'drop_rate_first_stages': mp.best_stage[item['name']]['drop_rate_first_stages'],
-                            'last_updated': update_time}})
-          else:
-              collection.update_one({'_id': item['_id']},
-                  {'$set': {'green_ticket_value': Decimal128('%.3f'%(mp.greenTickets[item['name']])),
-                            'Notes': mp.Notes[item['name']],
-                            'lowest_ap_stages': [{}],
-                            'balanced_stages': [{}],
-                            'drop_rate_first_stages': [{}],
-                            'last_updated': update_time}})
-      if 'golden_ticket_value' in item:
-          collection.update_one({'_id': item['_id']},
-                  {'$set': {'golden_ticket_value': Decimal128('%.3f'%(mp.yellowTickets[item['name']])),
-                            'Notes': mp.Notes[item['name']],
-                            'last_updated': update_time}})
+    print('正在更新数据库' + str(collection.name))
+    for item in collection.find():
+        if 'credit_store_value' in item:
+            if item['name'] in mp.best_stage:
+                collection.update_one({'_id': item['_id']},
+                    {'$set': {'credit_store_value': Decimal128('%.3f'%(100*mp.creditEffect[item['name']])),
+                              'Notes': mp.Notes[item['name']],
+                              'lowest_ap_stages': mp.best_stage[item['name']]['lowest_ap_stages'],
+                              'balanced_stages': mp.best_stage[item['name']]['balanced_stages'],
+                              'drop_rate_first_stages': mp.best_stage[item['name']]['drop_rate_first_stages'],
+                              'last_updated': update_time}
+                    })
+            else:
+                collection.update_one({'_id': item['_id']},
+                    {'$set': {'credit_store_value': Decimal128('%.3f'%(100*mp.creditEffect[item['name']])),
+                              'Notes': mp.Notes[item['name']],
+                              'lowest_ap_stages': [{}],
+                              'balanced_stages': [{}],
+                              'drop_rate_first_stages': [{}],
+                              'last_updated': update_time}
+                    })
+        if 'green_ticket_value' in item:
+            if item['name'] in mp.best_stage:
+                collection.update_one({'_id': item['_id']},
+                    {'$set': {'green_ticket_value': Decimal128('%.3f'%(mp.greenTickets[item['name']])),
+                              'Notes': mp.Notes[item['name']],
+                              'lowest_ap_stages': mp.best_stage[item['name']]['lowest_ap_stages'],
+                              'balanced_stages': mp.best_stage[item['name']]['balanced_stages'],
+                              'drop_rate_first_stages': mp.best_stage[item['name']]['drop_rate_first_stages'],
+                              'last_updated': update_time}})
+            else:
+                collection.update_one({'_id': item['_id']},
+                    {'$set': {'green_ticket_value': Decimal128('%.3f'%(mp.greenTickets[item['name']])),
+                              'Notes': mp.Notes[item['name']],
+                              'lowest_ap_stages': [{}],
+                              'balanced_stages': [{}],
+                              'drop_rate_first_stages': [{}],
+                              'last_updated': update_time}})
+        if 'golden_ticket_value' in item:
+            collection.update_one({'_id': item['_id']},
+                    {'$set': {'golden_ticket_value': Decimal128('%.3f'%(mp.yellowTickets[item['name']])),
+                              'Notes': mp.Notes[item['name']],
+                              'last_updated': update_time}})
 
-  print(str(collection.name)+'更新完成.')
+    print(str(collection.name)+'更新完成.')
 
 mp = MaterialPlanning(filter_stages=['S4-4', 'S6-4'],
                       filter_freq=100,
@@ -75,14 +76,14 @@ mp.get_plan(required_dct, owned_dct, print_output=False, outcome=True,
                                   gold_demand=True, exp_demand=True)
 [mp.output_best_stage(x) for x in '123']
 if Event_Stages:
-  # 如果活动开放, 就先更新带活动关卡的, 再更新正常关卡
-  # 如果活动不开放, 上面算的就是正常关卡
-  update_collection(db_Event)
-  mp = MaterialPlanning(filter_stages=['S4-4', 'S6-4'] + Event_Stages,
+    # 如果活动开放, 就先更新带活动关卡的, 再更新正常关卡
+    # 如果活动不开放, 上面算的就是正常关卡
+    update_collection(db_Event)
+    mp = MaterialPlanning(filter_stages=['S4-4', 'S6-4'] + Event_Stages,
                       filter_freq=100,
                       update=False,
                       printSetting='0000110010',
                       )
-  mp.get_plan(required_dct, owned_dct, print_output=False, outcome=True,
+    mp.get_plan(required_dct, owned_dct, print_output=False, outcome=True,
                                   gold_demand=True, exp_demand=True)
 update_collection(db_Material)
