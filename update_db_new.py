47d61dd893dfd824c7751032a07fe6905889385e
SQRPI <sqr_pi@163.com>
2020-03-17 23:56:14 +0800

    add 危机合约

--- update_db_new.py
+++ update_db_new.py
@@ -11,7 +11,7 @@ import time
 from dateutil import parser
 from utils import required_dct, owned_dct
 
-print('正在更新数据库...')
+print('正在从企鹅物流获取数据...')
 server = open('data/server.txt', 'r').readline().strip()
 dbclient = pymongo.MongoClient(server)
 db = dbclient['Arknights_OneGraph']
@@ -21,15 +21,15 @@ Event_Stages = ['SA-%d'%x for x in range(1,6)]
 update_time = parser.parse(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
 mp_event = MaterialPlanning(filter_stages=['S4-4', 'S6-4'],
                       filter_freq=100,
-                      update=True,
-                      printSetting='0000110010'
+                      update=False,
+                      printSetting='0000110011'
                       )
 mp_event.get_plan(required_dct, owned_dct, print_output=False, outcome=True,
                                   gold_demand=True, exp_demand=True)
 mp = MaterialPlanning(filter_stages=['S4-4', 'S6-4'] + Event_Stages,
                       filter_freq=100,
                       update=False,
-                      printSetting='0000110010'
+                      printSetting='0000110011'
                       )
 mp.get_plan(required_dct, owned_dct, print_output=False, outcome=True,
                                   gold_demand=True, exp_demand=True)
@@ -39,6 +39,13 @@ mp.get_plan(required_dct, owned_dct, print_output=False, outcome=True,
 
 print('正在更新数据库')
 for item in collection.find():
+    x = item['name']
+    print('\r已更新%s\t' % x, end='\t')
+    if item['name'] in mp.HeYueDict or item['name'] in mp.HYODict:
+        collection.update_one({'_id': item['_id']},
+                              {'$set': {'contingency_store_value': {'finite': '%.3f'%mp.HeYueDict[x] if x in mp.HeYueDict else '0.0',
+                                                                    'infinite': '%.3f'%mp.HYODict[x] if x in mp.HYODict else '0.0'}}
+                })
     if 'credit_store_value' in item:
         if item['name'] in mp.best_stage:
             collection.update_one({'_id': item['_id']},
