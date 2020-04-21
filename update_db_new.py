a45ec4285b060888775b4aff9ed289fc54b03cf5
SQRPI <sqr_pi@163.com>
2020-04-21 20:49:50 +0800

    生于黑夜活动

--- update_db_new.py
+++ update_db_new.py
@@ -17,16 +17,16 @@ dbclient = pymongo.MongoClient(server)
 db = dbclient['Arknights_OneGraph']
 
 collection = db['Material_Event']
-Event_Stages = ['SA-%d'%x for x in range(1,6)]
+Event_Stages = ['DM-%d'%x for x in range(1,9)]
 update_time = parser.parse(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()))
-mp_event = MaterialPlanning(filter_stages=['S4-4', 'S6-4'],
+mp_event = MaterialPlanning(filter_stages=['S4-4', 'S6-4'] + Event_Stages,
                       filter_freq=100,
-                      update=True,
-                      printSetting='0000110011'
+                      update=False,
+                      printSetting='0000111011'
                       )
 mp_event.get_plan(required_dct, owned_dct, print_output=False, outcome=True,
                                   gold_demand=True, exp_demand=True)
-mp = MaterialPlanning(filter_stages=['S4-4', 'S6-4'] + Event_Stages,
+mp = MaterialPlanning(filter_stages=['S4-4', 'S6-4'],
                       filter_freq=100,
                       update=False,
                       printSetting='0000110011'
@@ -40,12 +40,7 @@ mp.get_plan(required_dct, owned_dct, print_output=False, outcome=True,
 print('正在更新数据库')
 for item in collection.find():
     x = item['name']
-    print('\r已更新%s\t' % x, end='\t')
-    if item['name'] in mp.HeYueDict or item['name'] in mp.HYODict:
-        collection.update_one({'_id': item['_id']},
-                              {'$set': {'contingency_store_value': {'infinite': '%.3f'%mp.HeYueDict[x] if x in mp.HeYueDict else '0.0',
-                                                                    'finite': '%.3f'%mp.HYODict[x] if x in mp.HYODict else '0.0'}}
-                })
+    #print('\r已更新%s\t' % x, end='\t')
     if 'credit_store_value' in item:
         if item['name'] in mp.best_stage:
             collection.update_one({'_id': item['_id']},
