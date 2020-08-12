80e8a22806e4540d54082a26e1cb7d66cf4c5b6c
Strontium233 <metako213@gmail.com>
2020-08-11 21:49:50 -0400

    feat(add) purple store calculations

--- update_db_new.py
+++ update_db_new.py
@@ -32,13 +32,6 @@ mp_event = MaterialPlanning(filter_stages=Filter_special_stages + Filter_special
 mp_event.get_plan(required_dctCN, owned_dct, print_output=False, outcome=True,
                                   gold_demand=True, exp_demand=True)
 
-mp_event = MaterialPlanning(filter_stages=Filter_special_stages + Filter_special_items,
-                      filter_freq=100,
-                      update=True,
-                      printSetting='000000000001',CCSeason=2
-                      )
-mp_event.get_plan(required_dctCN, owned_dct, print_output=False, outcome=True,
-                                  gold_demand=True, exp_demand=True)
 mp = MaterialPlanning(filter_stages=Filter_special_stages + Filter_special_items + Event_Stages,
                       filter_freq=100,
                       update=True,
